95571839699714a581998f02ee757a33962bbb98
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-26 08:34:08 -0400

    add removeVertex method

--- src/Graphs/Graphs.ts
+++ src/Graphs/Graphs.ts
@@ -5,6 +5,7 @@ interface GraphShape {
   vertices: Vertex[];
   addVertex: (data: string) => Vertex;
   print: () => void;
+  removeVertex: (vertexToRemove: Vertex) => void;
 }
 
 class Graph implements GraphShape {
@@ -19,6 +20,11 @@ class Graph implements GraphShape {
     return vertex;
   }
 
+  removeVertex(vertexToRemove: Vertex) {
+    const filteredVertices = this.vertices.filter((vertex: Vertex) => vertex !== vertexToRemove);
+    this.vertices = filteredVertices;
+  }
+
   print() {
     this.vertices.forEach((vertex: Vertex) => vertex.print());
   }
@@ -28,6 +34,7 @@ const trainNetwork = new Graph();
 const atlantaStation = trainNetwork.addVertex('Atlanta');
 const newYorkStation = trainNetwork.addVertex('New York');
 console.log(atlantaStation, newYorkStation);
+trainNetwork.removeVertex(atlantaStation);
 trainNetwork.print();
 
 module.exports = Graph;
