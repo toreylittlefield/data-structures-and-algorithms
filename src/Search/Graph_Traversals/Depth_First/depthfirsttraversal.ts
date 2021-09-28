0dfa90da081abcd952d3654f9e776aca8cba860b
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-28 04:32:04 -0400

    add simpleGraph from new Graph instance

--- src/Search/Graph_Traversals/Depth_First/depthfirsttraversal.ts
+++ src/Search/Graph_Traversals/Depth_First/depthfirsttraversal.ts
@@ -1,3 +1,15 @@
-import simpleGraph from './simpleGraph';
+import { Vertex } from '../../../Graphs/Vertex';
+import testGraph from './simpleGraph';
 
-console.log(simpleGraph);
+type DepthFirstTraversal = (start: Vertex) => void;
+
+const depthFirstTraversal: DepthFirstTraversal = (start) => {
+  console.log(start.data);
+  if (start.edges.length) {
+    const neighbor = start.edges[0].end;
+    console.log(neighbor);
+    depthFirstTraversal(neighbor.edges[0]);
+  }
+};
+
+depthFirstTraversal(testGraph.vertices[0]);
