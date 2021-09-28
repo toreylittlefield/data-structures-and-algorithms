04474b584a9db91e4a72c119834f0549ba86c5f2
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-28 04:49:59 -0400

    replace if with iterator forEach loop

--- src/Search/Graph_Traversals/Depth_First/depthfirsttraversal.ts
+++ src/Search/Graph_Traversals/Depth_First/depthfirsttraversal.ts
@@ -1,15 +1,21 @@
 import { Vertex } from '../../../Graphs/Vertex';
 import testGraph from './simpleGraph';
 
-type DepthFirstTraversal = (start: Vertex) => void;
+type DepthFirstTraversal = (start: Vertex, visitedVertices?: Vertex[]) => void;
 
-const depthFirstTraversal: DepthFirstTraversal = (start) => {
+const depthFirstTraversal: DepthFirstTraversal = (start, visitedVertices = [start]) => {
   console.log(start.data);
-  if (start.edges.length) {
-    const neighbor = start.edges[0].end;
-    console.log(neighbor);
-    depthFirstTraversal(neighbor.edges[0]);
-  }
+  //   if (start.edges.length) {
+  start.edges.forEach((edge) => {
+    // const neighbor = start.edges[0].end;
+    const neighbor = edge.end;
+
+    if (!visitedVertices.includes(neighbor)) {
+      visitedVertices.push(neighbor);
+      depthFirstTraversal(neighbor, visitedVertices);
+    }
+  });
+  //   }
 };
 
 depthFirstTraversal(testGraph.vertices[0]);
