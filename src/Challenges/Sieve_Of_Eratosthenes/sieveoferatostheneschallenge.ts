1587def7090d5040033fe74942ef90088ca04291
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-28 22:15:41 -0400

    find primes sieve of erathpsthene challenge solution time complexity theta n log n

--- src/Challenges/Sieve_Of_Eratosthenes/sieveoferatostheneschallenge.ts
+++ src/Challenges/Sieve_Of_Eratosthenes/sieveoferatostheneschallenge.ts
@@ -4,4 +4,24 @@
 // prime number is a positive number with no divisors but 1 and itself. 2, 3, 11, and 443 are all prime numbers.
 // sieve assumes {2,...,n} are prime, and then successively marking them as NOT prime.
 
-const sieveArray = [...Array(99).keys()].map((key) => key + 2);
+const sieveArray: number[] = [...Array(100).keys()].map((key) => key + 2);
+
+type FindPrimes = (arrayNums: number[]) => number[];
+
+type FindMultiples = (arrayNums: number[], primes?: number[]) => number[];
+
+const findPrimes: FindPrimes = (arrayNums) => {
+  const findMultiples: FindMultiples = (arr, primes = []) => {
+    if (arr.length === 0) return primes;
+    const numToEval = arr[0];
+    primes.push(numToEval);
+    arr = arr.filter((num) => {
+      // console.log(arr.length)
+      return num % numToEval !== 0;
+    });
+    return findMultiples(arr, primes);
+  };
+  return findMultiples(arrayNums);
+};
+const res = findPrimes(sieveArray);
+console.log(res.length);
