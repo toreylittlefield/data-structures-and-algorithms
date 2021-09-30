8ca48de5d29839c6ee185dc03d46f7223828bf3d
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-29 20:38:32 -0400

    recursive stair challenege (fibo / memo)

---
+++ src/Challenges/Recursive Stairs/recursivestairs.ts
@@ -0,0 +1,25 @@
+// find number of ways to climb  the stairs of height k given n[] the order in which the stairs can be climbed
+// basically how many ways (permutations) to create the sum of k given an array of numbers
+
+const testHeight = 4;
+const testAllowed = [2, 4, 1, 3, 0];
+
+// 1111, 121, 121, 211, 22, 31, 13
+
+const numberWaysToClimb = (height: number, allowedSteps: number[]) => {
+  if (height === 0 || height === 1) return 1;
+  let uniqueAllowed: number[] = [...new Set(allowedSteps)];
+  let stairs: { [key: string]: number } = {};
+  stairs[0] = 1;
+  for (let stairIdx = 1; stairIdx <= height; stairIdx++) {
+    stairs[stairIdx] = 0;
+    uniqueAllowed.forEach((allowedCase) => {
+      if (allowedCase === 0) return;
+      if (stairs[stairIdx - allowedCase]) stairs[stairIdx] += stairs[stairIdx - allowedCase];
+    });
+  }
+  console.log(stairs);
+  return stairs[height];
+};
+
+console.log(numberWaysToClimb(testHeight, testAllowed));
