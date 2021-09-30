345e462e90eba4065d7d2e834a38202c33c1bfd6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-30 00:26:43 -0400

    fix N <=1

--- src/Challenges/Dynamic_Programming/dynamicprogramming.ts
+++ src/Challenges/Dynamic_Programming/dynamicprogramming.ts
@@ -1,8 +1,8 @@
 const memofibonacci = () => {
   const memo: { [key: string]: number } = {};
   return function fibonacci(n: number) {
-    if (n === 0 || n === 1) {
-      return 1;
+    if (n <= 1) {
+      return n;
     }
     if (n in memo) return memo[n];
     memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
@@ -13,5 +13,5 @@ const memofibonacci = () => {
 
 // Test your code with calls here:
 const fibo = memofibonacci();
-console.log(fibo(19));
+console.log(fibo(20));
 console.log(fibo(200));
