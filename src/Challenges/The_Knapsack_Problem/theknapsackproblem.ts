a66cf71f372afabc30bcada91d1b729fac824707
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-30 01:52:32 -0400

    the knapsack problem dynamic programming and recursion

---
+++ src/Challenges/The_Knapsack_Problem/theknapsackproblem.ts
@@ -0,0 +1,6 @@
+/**
+ * Imagine that you’re a thief breaking into a house. There are so many valuables to steal
+ * - diamonds, gold, jewelry, and more! But remember, you’re just one person who can only carry so much.
+ * Each item has a weight and value, and your goal is to maximize the total value of items while
+ * remaining within the weight limit of your knapsack.
+ */
