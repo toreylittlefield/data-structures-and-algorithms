2cc857017cee1946bd33eb0c13ff3847e77011be
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-09-29 21:13:12 -0400

    refactor

--- src/Challenges/Recursive Stairs/recursivestairs.ts
+++ src/Challenges/Recursive Stairs/recursivestairs.ts
@@ -2,23 +2,20 @@
 // basically how many ways (permutations) to create the sum of k given an array of numbers
 
 const testHeight = 4;
-const testAllowed = [2, 4, 1, 3, 0];
-
-// 1111, 121, 121, 211, 22, 31, 13
+const testAllowed = [2, 4, 1, 3];
 
 const numberWaysToClimb = (height: number, allowedSteps: number[]) => {
   if (height === 0 || height === 1) return 1;
   let uniqueAllowed: number[] = [...new Set(allowedSteps)];
   let stairs: { [key: string]: number } = {};
   stairs[0] = 1;
-  for (let stairIdx = 1; stairIdx <= height; stairIdx++) {
+  for (let stairIdx = 1; stairIdx < height + 1; stairIdx++) {
     stairs[stairIdx] = 0;
     uniqueAllowed.forEach((allowedCase) => {
       if (allowedCase === 0) return;
       if (stairs[stairIdx - allowedCase]) stairs[stairIdx] += stairs[stairIdx - allowedCase];
     });
   }
-  console.log(stairs);
   return stairs[height];
 };
 
